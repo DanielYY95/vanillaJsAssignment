7e87c987d25c75de884e95d61a15e100b2f35be7
YangChoMyeong <84573015+YangChoMyeong@users.noreply.github.com>
2021-07-14 12:47:20 +0900

    KeepgoingGraduate

--- Clock.js
+++ Clock.js
@@ -1,13 +1,12 @@
-const Clock=document.body.querySelector("#clock");
+const Clock = document.body.querySelector("#clock");
 
+function GetTime() {
+  const date = new Date();
+  const Hours = String(date.getHours()).padStart(2, "0");
+  const Minutes = String(date.getMinutes()).padStart(2, "0");
+  const Seconds = String(date.getSeconds()).padStart(2, "0");
+  Clock.innerText = `${Hours}:${Minutes}:${Seconds}`;
+}
 
-function GetTime(){
-    const date=new Date();
-    const Hours=String(date.getHours()).padStart(2,"0");
-    const Minutes=String(date.getMinutes()).padStart(2,"0");
-    const Seconds=String(date.getSeconds()).padStart(2,"0");
-    Clock.innerText=`${Hours}:${Minutes}:${Seconds}`;
-    }
-    
 GetTime();
-setInterval(GetTime,1000);
+setInterval(GetTime, 1000);
--- Username.js
+++ Username.js
@@ -1,18 +1,14 @@
-const NameInput=document.body.querySelector("#name");
-const NAMEFORM=document.body.querySelector("#nameform");
-const Greeting=document.body.querySelector("#greet");
-const HIDDEN_CLASSNAME="hidden";
+const NameInput = document.body.querySelector("#name");
+const NAMEFORM = document.body.querySelector("#nameform");
+const Greeting = document.body.querySelector("#greet");
+const HIDDEN_CLASSNAME = "hidden";
 
-
-function onLoginSubmit(event){
-
-    event.preventDefault(event);
-    NAMEFORM.classList.add(HIDDEN_CLASSNAME);
-    const Username=NameInput.value;
-    localStorage.setItem("USERNAME_KEY",Username);
-    Greeting.innerText=`What's up? Is your coin okay? Mr.${Username}`;
-
-
+function onLoginSubmit(event) {
+  event.preventDefault(event);
+  NAMEFORM.classList.add(HIDDEN_CLASSNAME);
+  const Username = NameInput.value;
+  localStorage.setItem("USERNAME_KEY", Username);
+  Greeting.innerText = `What's up? Is your coin okay? Mr.${Username}`;
 }
 
-NAMEFORM.addEventListener("submit",onLoginSubmit);
+NAMEFORM.addEventListener("submit", onLoginSubmit);
