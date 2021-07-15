d8a693c8342baf4874ae9ff6c7542d08c9807451
YangChoMyeong <84573015+YangChoMyeong@users.noreply.github.com>
2021-07-15 17:59:41 +0900

    Create Toinvest.js
    오류- 입력하면 화면에서 하나 추가되고, 하나 삭제되고 ...
    7-6부터 잘 안되는 것 같다...

---
+++ Toinvest.js
@@ -0,0 +1,59 @@
+const investCoinForm= document.body.querySelector("#coinForm");
+const investCoinInput= document.body.querySelector("#coinInput");
+const LIST=document.body.querySelector("#list");
+const COINLIST="CoinList"; 
+
+let CoinList=[]; //array 형식으로 저장해야한다. 그리고 이전과 새로운 것들을 유지시키기위해 계속해서 업데이트하는 형식으로
+
+function paintCoin(newCoinObj){
+  const li = document.createElement("li");
+  li.id=newCoinObj.id;
+  const span = document.createElement("span");
+  span.innerText = newCoinObj.text; 
+  const button = document.createElement("button");
+  button.innerText="❌"; //이모지: 윈도우 + . 
+  button.addEventListener("click", deleteCoin);
+  li.appendChild(span); 
+  li.appendChild(button);
+  LIST.appendChild(li);
+}
+
+function saveCoin(){
+  localStorage.setItem(COINLIST,JSON.stringify(CoinList));
+} // string 형태로 저장
+
+function deleteCoin(event){
+  const li= event.target.parentElement; //target이 대상의 부모
+  li.remove();
+  CoinList=CoinList.filter((toDo)=>toDo.id!==parseInt(li.id));
+  saveCoin();
+}
+
+
+function coinInvest(event){
+  event.preventDefault();
+  const CoinInput= investCoinInput.value;
+  investCoinInput.value="";
+  const newCoinObj={
+    text:CoinInput, id:Date.now()  // array에 id와 text 부여
+  };
+  CoinList.push(newCoinObj); //먼저 array에 던져 넣고.
+  paintCoin(newCoinObj);
+  saveCoin(); // array 자체로 계속 저장해주는 것이다. 
+} // 이 함수는 계속해서 제출할 때마다 값을 저장하고 화면에 보여주는 것까지!! 
+
+function CoinFilter(){
+  return true;
+}
+
+
+investCoinForm.addEventListener("submit", coinInvest);
+
+const savedCoin=localStorage.getItem(COINLIST);
+
+if (savedCoin !==null){ //저장은 되지만 계속 화면에 나오게끔//
+	const parsedCoin=JSON.parse(savedCoin); // array[1]=? 가 되게끔 하는 것이다. 인덱스 부여된다. 
+	CoinList=parsedCoin; //인덱스 부여된 이전 것들을 계속해서 CoinList에 업데이트하고,,, 이 변수가 쓰일 때 미리 준비가 되는 것//
+	parsedCoin.forEach(paintCoin);
+}
+
