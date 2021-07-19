a850697bf4e928e0b50c2a79fd47a6c1c0a8f866
YangChoMyeong <84573015+YangChoMyeong@users.noreply.github.com>
2021-07-19 14:22:44 +0900

    Update Weather.js
    null 값과 관련된 오류가 생긴다면 console.log 로 천천히 훑고, html에서 문제가 생긴 것일 수도 있으니 주의할 것.

--- Weather.js
+++ Weather.js
@@ -1,22 +1,20 @@
-const City=document.body.querySelector("#weather span:first-child");
-const Temp = document.querySelector("#weather span:nth-child(2)");
-const	Weather=document.body.querySelector("#weather span:last-child"); 
+const City=document.querySelector("#city");
+const Temp = document.querySelector("#temp");
+const	Weather=document.querySelector("#weather"); 
 const API_key="d85c8aa957c477ee3554b9b7f8d54c2a";
-
-
 
 function onGeoOk(position){ //positon은 geolocation 하고나서 얻을 수 있는 object
   const lat=position.coords.latitude; // position object 내  property
   const lon=position.coords.longitude; //
-  const url= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
+  const url= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric&lang=kr`;
   fetch(url) //fetch: url을 불러오기
-	.then((response)=>response.json()) //불러오고나서 반응하는것의 json을 return 받는다.
-	.then((data)=>{ //그리고나서 return 받고나서 json의 data를 얻었다면
-	  City.innerText = data.name + ",";
-    Weather.innerText = data.weather[0].main + ","; /// 여기서 자꾸,,,,, 
-    Temp.innerText = data.main.temp + "°C";
-  }); //왜 계속 Uncaught (in promise) TypeError: Cannot set property 'innerText' of null at Weather.js:15// 가 나오는가...???
-  
+	.then(response=>response.json()) //불러오고나서 반응하는것의 json을 return 받는다.
+	.then(data=>{ //그리고나서 return 받고나서 json의 data를 얻었다면
+	  City.innerText = data.name + ", ";
+    Temp.innerText = data.main.temp + "°C, ";
+    Weather.innerText = data.weather[0].main;
+  }); 
+
 }
 
 function onGeoError(){
@@ -24,3 +22,8 @@ function onGeoError(){
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
+
+
+/// 아예 html에서 span id 부여
+/// null 값 같은 오류가 뜬다면, console.log로 천천히...
+/// html에서 문제가 생긴 걸 수도 있다. 
