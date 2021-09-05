c81ea617fa32cd48779578ab76c66dc0dea9f473
YangChoMyeong <84573015+YangChoMyeong@users.noreply.github.com>
2021-09-06 00:03:57 +0900

    Update Toinvest.js

--- Toinvest.js
+++ Toinvest.js
@@ -3,7 +3,7 @@ const investCoinInput= document.body.querySelector("#coinInput");
 const LIST=document.body.querySelector("#list");
 const COINLIST="CList"; 
 
-let CList=[]; //array 형식으로 저장해야한다. 그리고 이전과 새로운 것들을 유지시키기위해 계속해서 업데이트하는 형식으로
+let CList=[]; 
 
 function paintCoin(newCoin){
   const li = document.createElement("li");
@@ -11,7 +11,7 @@ function paintCoin(newCoin){
   const span = document.createElement("span");
   span.innerText = newCoin.text+"          "; 
   const button = document.createElement("button");
-  button.innerText="❌"; //이모지: 윈도우 + . 
+  button.innerText="❌"; 
   button.addEventListener("click", deleteCoin);
   li.appendChild(span); 
   li.appendChild(button);
@@ -20,10 +20,10 @@ function paintCoin(newCoin){
 
 function saveCoin(){
   localStorage.setItem(COINLIST,JSON.stringify(CList));
-} // string 형태로 저장
+} 
 
 function deleteCoin(event){
-  const li= event.target.parentElement; //target이 대상의 부모
+  const li= event.target.parentElement; 
   li.remove();
   CList=CList.filter((toDo)=>toDo.id !==parseInt(li.id));
   saveCoin();
@@ -33,13 +33,13 @@ function deleteCoin(event){
 function coinInvest(event){
   event.preventDefault();
   const newCoin={
-    text:investCoinInput.value, id:Date.now()  // array에 id와 text 부여
+    text:investCoinInput.value, id:Date.now() 
   };
   investCoinInput.value="";
-  CList.push(newCoin); //먼저 array에 던져 넣고.
+  CList.push(newCoin); 
   paintCoin(newCoin);
-  saveCoin(); // array 자체로 계속 저장해주는 것이다. 
-} // 이 함수는 계속해서 제출할 때마다 값을 저장하고 화면에 보여주는 것까지!! 
+  saveCoin(); 
+} 
 
 
 
@@ -47,8 +47,8 @@ investCoinForm.addEventListener("submit", coinInvest);
 
 const savedCoin=localStorage.getItem(COINLIST);
 
-if (savedCoin !==null){ //저장은 되지만 계속 화면에 나오게끔//
-	const parsedCoin=JSON.parse(savedCoin); // array[1]=? 가 되게끔 하는 것이다. 인덱스 부여된다. 
-	CList=parsedCoin; //인덱스 부여된 이전 것들을 계속해서 CoinList에 업데이트하고,,, 이 변수가 쓰일 때 미리 준비가 되는 것//
+if (savedCoin !==null){
+	const parsedCoin=JSON.parse(savedCoin); 
+	CList=parsedCoin;
 	parsedCoin.forEach(paintCoin);
 }
